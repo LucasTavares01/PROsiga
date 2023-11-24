4006a79eccb2e8311c88108b3143bba6d9cd50e8
LucasTavares01 <127222845+LucasTavares01@users.noreply.github.com>
2023-11-24 12:19:27 -0300

    Várias mudanças na pagina aulas

--- WebPROsiga/aulas.php
+++ WebPROsiga/aulas.php
@@ -2,20 +2,7 @@
 require($_SERVER['DOCUMENT_ROOT'] . '/PROsiga/Backend/Controles/controleSessao.php');
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST['botaomateria'])) {
-        $materias = $_SESSION['materias'];
-        $cod = $_POST['cod'];
-        $_SESSION['cod_mat'] = $cod;
-        foreach ($materias as $materia) {
-            if ($materia->cod_materia == $cod) {
-                ControleSessao::selecionarMateria($materia);
-                header("Location: aulas.php");
-                exit(); // Importante sair do script depois do redirecionamento
-            }
-        }
-    }
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -56,36 +43,52 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <main>
             <h2>AULAS</h2>
             <div class="cabecalhomateria">
+                <div class="fundocabecalhomateria"></div>
                 <?php                
                 $materia = $_SESSION['materia'];
                 $imagemCodificada = base64_encode($materia->icone);
                 echo "<img class='icone' src='data:image/svg+xml;base64,$imagemCodificada' />";
                 echo"<span class='nomemateria'>$materia->nome</span>";
                 ?>
+
+                
+            </div>
+                <div class="cabecalhoitens">
+                <p class="data">DATA</p>
+                <p class="nome">NOME</p>
+                <p class="concluida">CONCLUÍDA</p>
             </div>
 
-            <div class="cabecalhoitens">
-                <p>DATA<p>
-                <p>NOME<p>
-                <p>CONCLUÍDA<p>
-            </div>
                 
 
 
                 <div class='caixabotaoaulas'>
                     <?php
-                    $materias = $_SESSION['materias'];
-                    foreach ($materias as $materia) {
-                        $imagemCodificada = base64_encode($materia->icone);
-                        echo "<form action='' method='post'>";
-                        echo "<input type='hidden' name='cod' value='$materia->cod_materia' />";
-                        echo "<button type='submit' name='botaomateria' class='botaomateria'>";
-                        echo "<img class='icone' src='data:image/svg+xml;base64,$imagemCodificada' />";
-                        echo "<span class='nomebotao'>$materia->nome</span>";
-                        echo "</button>";
-                        echo "</form>";
-                    }
+                     $aulas = $_SESSION['aulas'];
+                     foreach ($aulas as $aula) {
+                         echo "<form action='' method='post'>";
+
+                    //     echo "<input type='hidden' name='cod' value='$materia->cod_materia' />";
+
+                         echo "<button type='submit' name='botaoaula' class='botaoaula'>";
+                         echo "<p class='datab'>$aula->data</p>";
+                         echo "<p class='nomeb'>$aula->titulo</p>";
+                         echo "<p class='concluidab'>$aula->status</p>";
+                         echo "</button>";
+                        
+                         echo "</form>";
+                     }
                     ?>
+
+
+
+
+
+
+
+
+
+
                 </div>            
         </main>
 
@@ -104,7 +107,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <?php
 // if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-//     if (isset($_POST['botaomateria'])) {
+//     if (isset($_POST['botaoaulas'])) {
 //         $materias = [];
 //         $materias = $_SESSION['materias'];
 //         $cod = $_POST['cod'];
