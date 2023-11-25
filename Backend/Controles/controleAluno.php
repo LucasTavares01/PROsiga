052c65624bfa2e63f4c89e49bec782ab78b65be9
sam <116354478+srslopes@users.noreply.github.com>
2023-11-25 15:38:15 -0300

    Adição do atributo status ao aluno

--- Backend/Controles/controleAluno.php
+++ Backend/Controles/controleAluno.php
@@ -6,13 +6,14 @@ class ControleAluno {
         $aluno = null;
         $bd = BancoDeDados::obterInstancia();
         $cod_mat = $_SESSION['materia']->cod_materia;
-        $resultados = $bd->consultar("SELECT ALUNO.ID_ALUNO, MATRICULA.ID_MATR, ALUNO.RA, ALUNO.NOME, ALUNO.IMG FROM ALUNO INNER JOIN MATRICULA ON MATRICULA.ID_ALUNO = ALUNO.ID_ALUNO WHERE ALUNO.ID_ALUNO = $id_aluno AND MATRICULA.COD_MAT = $cod_mat");
+        $resultados = $bd->consultar("SELECT ALUNO.ID_ALUNO, MATRICULA.ID_MATR, ALUNO.RA, ALUNO.NOME, ALUNO.IMG, ALUNO.STATUS FROM ALUNO INNER JOIN MATRICULA ON MATRICULA.ID_ALUNO = ALUNO.ID_ALUNO WHERE ALUNO.ID_ALUNO = $id_aluno AND MATRICULA.COD_MAT = $cod_mat");
         foreach ($resultados as $resultado) {
             $aluno = new Aluno($resultado['ALUNO.ID_ALUNO'],
             $resultado['MATRICULA.ID_MATR'],
             $resultado['ALUNO.RA'],
             $resultado['ALUNO.NOME'],
-            $resultado['ALUNO.IMG']);
+            $resultado['ALUNO.IMG'],
+            $resultado['ALUNO.STATUS']);
             break;
         }
         return $aluno;
@@ -21,13 +22,14 @@ class ControleAluno {
     public static function buscarAlunos($idAula) {
         $alunos = [];
         $bd = BancoDeDados::obterInstancia();
-        $resultados = $bd->consultar("SELECT ALUNO.ID_ALUNO, MATRICULA.ID_MATR, ALUNO.RA, ALUNO.NOME, ALUNO.IMG FROM ALUNO INNER JOIN MATRICULA ON MATRICULA.ID_ALUNO = ALUNO.ID_ALUNO INNER JOIN AULA ON AULA.COD_MAT = MATRICULA.COD_MAT WHERE AULA.ID_AULA = $idAula ORDER BY ALUNO.NOME");
+        $resultados = $bd->consultar("SELECT ALUNO.ID_ALUNO, MATRICULA.ID_MATR, ALUNO.RA, ALUNO.NOME, ALUNO.IMG, ALUNO.STATUS FROM ALUNO INNER JOIN MATRICULA ON MATRICULA.ID_ALUNO = ALUNO.ID_ALUNO INNER JOIN AULA ON AULA.COD_MAT = MATRICULA.COD_MAT WHERE AULA.ID_AULA = $idAula ORDER BY ALUNO.NOME");
         foreach ($resultados as $resultado) {
             $aluno = new Aluno($resultado['ALUNO.ID_ALUNO'],
             $resultado['MATRICULA.ID_MATR'],
             $resultado['ALUNO.RA'],
             $resultado['ALUNO.NOME'],
-            $resultado['ALUNO.IMG']);
+            $resultado['ALUNO.IMG'],
+            $resultado['ALUNO.STATUS']);
             $alunos[] = $aluno;
         }
         return $alunos;
--- Backend/Modelos/aluno.php
+++ Backend/Modelos/aluno.php
@@ -6,13 +6,15 @@ class Aluno {
     public $ra;
     public $nome;
     public $img;
+    public $status;
     
-    public function __construct($id_aluno, $id_matr, $ra, $nome, $img) {
+    public function __construct($id_aluno, $id_matr, $ra, $nome, $img, $status) {
         $this->id_aluno = $id_aluno;
         $this->id_matr = $id_matr;
         $this->ra = $ra;
         $this->nome = $nome;
         $this->img = $img;
+        $this->status = $status;
     }
 }
 ?>
